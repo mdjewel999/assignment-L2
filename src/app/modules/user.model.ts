afe045059759830e5cb23ae73f3338ac18f0cd96
KRComputer <mdjewel878761@gmail.com>
2023-12-01 02:50:54 +0600

    All code added

--- src/app/modules/user.model.ts
+++ src/app/modules/user.model.ts
@@ -2,74 +2,84 @@ import { Schema, model } from 'mongoose';
 import validator from 'validator';
 import { User } from './user/user.interface';
 
-const userSchema = new Schema<User>({
-  userId: {
-    type: Number,
-    required: true,
-    unique: true,
-  },
+const userSchema = new Schema<User>(
+  {
+    userId: {
+      type: Number,
+      required: true,
+      unique: true,
+    },
 
-  username: {
-    type: String,
-    required: true,
-  },
-
-  password: {
-    type: String,
-  },
-
-  fullName: {
-    firstName: {
+    username: {
       type: String,
       required: true,
     },
 
-    lastName: {
+    password: {
       type: String,
-      required: [true, 'Last name is required'],
-      validate:{
-        validator: (value: string) => validator.isAlpha(value),
-        message:'{VALUE} is not valid',
-      }
     },
-  },
 
-  age: {
-    type: Number,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  hobbies: {
-    type: String,
-    enum: ['reading', 'traveling', 'gardening'],
-  },
-  address: {
-    street: {
-      type: String,
+    fullName: {
+      firstName: {
+        type: String,
+        required: true,
+      },
+
+      lastName: {
+        type: String,
+        required: [true, 'Last name is required'],
+        validate: {
+          validator: (value: string) => validator.isAlpha(value),
+          message: '{VALUE} is not valid',
+        },
+      },
     },
-    city: {
-      type: String,
+
+    age: {
+      type: Number,
+      required: true,
+      validate: {
+        validator: (value: number) => value >= 18 && value <= 99,
+        message: 'Age must be between 18 and 99',
+      },
     },
-    country: {
+    email: {
       type: String,
+      required: true,
+      validate: {
+        validator: (value: string) => validator.isEmail(value),
+        message: '{VALUE} is not a valid email',
+      },
     },
-  },
-  orders: [
-    {
-      productName: {
+    hobbies: {
+      type: String,
+      enum: ['reading', 'traveling', 'gardening'],
+    },
+    address: {
+      street: {
         type: String,
       },
-      price: {
-        type: Number,
+      city: {
+        type: String,
       },
-      quantity: {
-        type: Number,
+      country: {
+        type: String,
       },
     },
-  ],
-});
-
+    orders: [
+      {
+        productName: {
+          type: String,
+        },
+        price: {
+          type: Number,
+        },
+        quantity: {
+          type: Number,
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
 export const UserModel = model<User>('User', userSchema);
